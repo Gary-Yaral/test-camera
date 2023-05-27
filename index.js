597edd37fb8720d7d2c9b459d58bcf3a5830e84c
Gary <gary.yaral@gmail.com>
2023-05-27 10:11:59 -0500

    second commit

--- index.js
+++ index.js
@@ -16,36 +16,19 @@ captureButton.addEventListener('click', function(e) {
   var canvasElement = document.getElementById('canvas');
   var canvasContext = canvasElement.getContext('2d');
 
-  // Establecer el tamaño deseado del canvas (más pequeño)
-  var canvasWidth = 320; // Ancho deseado
-  var canvasHeight = 240; // Altura deseada
+  // Establecer el tamaño del canvas al mismo que el video
+  canvasElement.width = videoElement.videoWidth;
+  canvasElement.height = videoElement.videoHeight;
 
-  // Calcular la escala de dibujo para mantener la relación de aspecto
-  var scaleX = videoElement.videoWidth / canvasWidth;
-  var scaleY = videoElement.videoHeight / canvasHeight;
-  var scale = Math.max(scaleX, scaleY);
-
-  // Calcular el tamaño final de la imagen capturada
-  var drawWidth = canvasWidth * scale;
-  var drawHeight = canvasHeight * scale;
-  var drawX = (videoElement.videoWidth - drawWidth) / 2;
-  var drawY = (videoElement.videoHeight - drawHeight) / 2;
-
-  // Asegurarse de que el canvas tenga el mismo tamaño que el video
-  canvasElement.width = drawWidth;
-  canvasElement.height = drawHeight;
-
-  // Dibujar el video en el canvas con la escala adecuada
-  canvasContext.drawImage(videoElement, drawX, drawY, drawWidth, drawHeight, 0, 0, canvasWidth, canvasHeight);
+  // Dibujar el video en el canvas
+  canvasContext.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
 
   // Obtener la imagen como un archivo base64 codificado en datos
   var imageDataURL = canvasElement.toDataURL('image/png');
 
-  // Mostrar la vista previa de la imagen capturada
-  var previewImageElement = document.getElementById('previewImage');
-  previewImageElement.src = imageDataURL;
-  
+
   // Asignar la imagen codificada al campo de entrada de archivo en el formulario
-  var fileInput = document.getElementById('myFile');
-  fileInput.value = imageDataURL;
+  /* var fileInput = document.getElementById('myFile');
+  fileInput.value = imageDataURL; */
 });
+
